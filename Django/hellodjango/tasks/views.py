b0bea48a87e062bb7d4c7ccbf64acd8ea6a16486
mvpsheng <2711136701@qq.com>
2023-06-06 11:21:20 +0800

    Add session in task app

--- Django/hellodjango/tasks/views.py
+++ Django/hellodjango/tasks/views.py
@@ -1,16 +1,17 @@
 from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
-
-
+# "foo", "bar", "baz"
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]  = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -18,7 +19,8 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
