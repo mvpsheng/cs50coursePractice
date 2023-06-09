adc85f5418eaeb9292098c5b0a81b4152b5b40f6
mvpsheng <2711136701@qq.com>
2023-06-09 21:48:46 +0800

    Use wrong valid flight function

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.test import Client, TestCase
+
+from django.db.models import Max
 
 # Create your models here.
 class Airport(models.Model):
@@ -17,6 +20,9 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
     
+    def is_valid_flight(self):
+        return self.origin != self.destination or self.duration > 0
+
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
     last = models.CharField(max_length=64)
